d0495fb43ec616c39de84225fe0dd437756c5f05
tokuteacher <tokuteacher@gmail.com>
2020-03-16 20:27:58 +0900

    add v-list-item to

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -39,16 +39,6 @@
                 {{ item.heading }}
               </v-subheader>
             </v-col>
-            <v-col
-              cols="6"
-              class="text-right"
-            >
-              <v-btn
-                to="/about"
-                small
-                text
-              >edit</v-btn>
-            </v-col>
           </v-row>
           <v-divider
             v-else-if="item.divider"
@@ -60,6 +50,7 @@
             v-else
             :key="i"
             link
+            :to="item.to"
           >
             <v-list-item-action>
               <v-icon>{{ item.icon }}</v-icon>
@@ -85,20 +76,10 @@ export default {
   data: () => ({
     drawer: null,
     items: [
-      { icon: 'home', text: 'home' },
-      { icon: 'touch_app', text: 'Reminders' },
-      { divider: true },
-      { heading: 'Labels' },
-      { icon: 'add', text: 'Create new label' },
-      { divider: true },
-      { icon: 'archive', text: 'Archive' },
-      { icon: 'delete', text: 'Trash' },
-      { divider: true },
-      { icon: 'settings', text: 'Settings' },
-      { icon: 'chat_bubble', text: 'Trash' },
-      { icon: 'help', text: 'Help' },
-      { icon: 'phonelink', text: 'App downloads' },
-      { icon: 'keyboard', text: 'Keyboard shortcuts' },
+      { icon: 'home', text: 'Home', to: '/' },
+      { icon: 'perm_identity', text: 'About me', to: '/about' },
+      { icon: 'work', text: 'Works', to: '/works' },
+      { icon: 'mail_outline', text: 'Contact', to: '/contact' },
     ],
   }),
 }
