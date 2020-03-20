606ea81cd9207d443a8e49e1913541e0a08cd357
tokuteacher <tokuteacher@gmail.com>
2020-03-20 20:47:36 +0900

    add works page

--- src/views/works.vue
+++ src/views/works.vue
@@ -1,17 +1,47 @@
 <template>
-  <div>
-    <p>ここはworks pageです。</p>
-    <p>ここはworks pageです。</p>
-    <p>ここはworks pageです。</p>
-    <p>ここはworks pageです。</p>
-    <p>ここはworks pageです。</p>
-    <p>ここはworks pageです。</p>
-    <p>ここはworks pageです。</p>
-  </div>
+  <v-container
+    fill-height
+    fluid
+    ma-auto
+    pa-0
+  >
+  <v-row
+    dense
+  >
+    <v-col cols="12">
+      <h2 class="display-4 text-center">WORKS</h2>
+      <h3 class="dispaly-3 text-center">実績</h3>
+    </v-col>
+    <v-col cols="12" class="col-md-4 col-sm-12">
+      <v-card color="blue" class="ma-3">
+        <v-card-title class="headline">Python</v-card-title>
+        <v-card-subtitle>Python Skills </v-card-subtitle>
+      </v-card>
+    </v-col>
+    <v-col cols="12" class="col-md-4 col-sm-6">
+      <v-card color="red" class="ma-3">
+        <v-card-title class="headline">Ruby</v-card-title>
+        <v-card-subtitle>Ruby Skills Here</v-card-subtitle>
+      </v-card>
+    </v-col>
+    <v-col cols="12" class="col-md-4 col-sm-6">
+      <v-card color="purple" class="ma-3">
+        <v-card-title class="headline">PHP</v-card-title>
+        <v-card-subtitle>PHP Skills Here</v-card-subtitle>
+      </v-card>
+    </v-col>
+  </v-row>
+  </v-container>
 </template>
 
 <script>
 </script>
 
-<style>
+<style scoped lang="scss">
+h2:not(.v-icon) {
+  font-family: "steeler", sans-serif!important;
+}
+.container {
+  max-width: 1024px;
+}
 </style>
