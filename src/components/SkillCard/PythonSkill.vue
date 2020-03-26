2b881284c98b41607e8382305671076a4b9aa35d
tokuteacher <tokuteacher@gmail.com>
2020-03-26 20:20:17 +0900

    edit skills component

---
+++ src/components/SkillCard/PythonSkill.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card class="ma-3" light>
+    <v-app-bar
+      dark
+      color="blue"
+    >
+      <v-toolbar-title>AI・データサイエンス</v-toolbar-title>
+    </v-app-bar>
+    <v-container>
+      <v-data-table
+        light
+        :headers="headers"
+        :items="skills"
+        class=""
+      hide-default-footer>
+      </v-data-table>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "PythonSkillItem",
+  props: {
+      source: String,
+  },
+  data: () => ({
+    headers: [{text: "技術", align: "end", sortable: false, value: "name"},
+              {text: "経験年数", value: "years", align: "end", sortable: false,},
+              {text: "level", value: "level", align: "end",sortable: false,}],
+    skills: [
+      { name: 'Python', years: 4, level: '★★★★★' },
+      { name: 'Python', years: 4, level: '★★' },
+      { name: 'Python', years: 4, level: '★★★★' },
+      { name: 'Python', years: 4, level: '★★★' },
+    ],
+  }),
+}
+</script>
--- src/views/works.vue
+++ src/views/works.vue
@@ -3,31 +3,16 @@
   <div class="container">
   <v-row class="title">
     <v-col cols="12">
-      <h2 class="display-4 text-center">WORKS</h2>
-      <h3 class="dispaly-3 text-center amber--text">実績</h3>
+      <h2 class="display-4 text-center">SKILLS</h2>
+      <h3 class="dispaly-3 text-center amber--text">技術</h3>
     </v-col>
   </v-row>
   <v-row>
-    <v-col cols="12" class="col-md-4 col-sm-12">
-      <v-card color="blue" class="ma-3" dark>
-        <v-card-title class="headline">AI・データサイエンス</v-card-title>
-        <!-- <v-card-subtitle color="white">Python Skills </v-card-subtitle>-->
-        <v-list light>
-          <v-list-item>Python </v-list-item>
-        </v-list>
-      </v-card>
+    <v-col cols="12" class="col-md-6">
+      <PythonSkill></PythonSkill>
     </v-col>
-    <v-col cols="12" class="col-md-4 col-sm-6">
-      <v-card color="red" class="ma-3" dark>
-        <v-card-title class="headline">Ruby</v-card-title>
-        <v-card-subtitle>Ruby Skills Here</v-card-subtitle>
-      </v-card>
-    </v-col>
-    <v-col cols="12" class="col-md-4 col-sm-6">
-      <v-card color="purple" class="ma-3" dark>
-        <v-card-title class="headline">PHP</v-card-title>
-        <v-card-subtitle>PHP Skills Here</v-card-subtitle>
-      </v-card>
+    <v-col cols="12" class="col-md-6">
+      <WebSkill></WebSkill>
     </v-col>
   </v-row>
   </div>
@@ -35,7 +20,16 @@
 </template>
 
 <script>
+import PythonSkill from '../components/SkillCard/PythonSkill'
+import WebSkill from '../components/SkillCard/WebSkill'
+
 export default {
+  name: 'Works',
+
+  components: {
+    PythonSkill,
+    WebSkill
+  },
   data () {
     return {
       section_style: {
