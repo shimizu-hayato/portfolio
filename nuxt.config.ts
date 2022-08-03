9d11d555e73d7d2c8e94d9948e3efc2bf81723dc
Hayato Shimizu <bigdanmcc@gmail.com>
2022-08-03 17:56:13 +0900

    edit nuxt.config.ts fot mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  mode: "spa",
+  ssr: false,
+  target: 'static',
   loadingIndicator: {
     name: 'rotating-plane',
     color: 'blue',
