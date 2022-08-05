57457a090af739ede26399bb66459c0d2ccd4ebc
Hayato Shimizu <bigdanmcc@gmail.com>
2022-08-05 22:22:58 +0900

    [WIP] add form submit

--- components/Contact.vue
+++ components/Contact.vue
@@ -32,7 +32,20 @@ section#contact(
             alt=""
             class="max-w-[768px]"
           )
-        form(id="contact-form")
+        form(
+          id="contact-form"
+          name="contact"
+          method="POST"
+          netlify 
+          data-netlify-honeypot="bot-field"
+          @submit.prevent="onSubmit"
+          :class="sendingClass"
+        )
+          input(
+            type="hidden"
+            name="form-name"
+            value="contact"
+          )
           label(
             for="form-name"
             class="block mb-2 text-sm uppercase"
@@ -181,6 +194,9 @@ section#contact(
             span(
               class="absolute text-xs text-alert-red mt-[0.4rem] invisible peer-invalid:visible"
             ) Message is empty.
+          div(v-show="false")
+            label(for="message") スパムではない場合は空欄
+            input(type="text" name="bot-field" v-model="botField")
           button(
             type="submit"
             class="btn btn-btn-outline px-5 py-2 flex items-center"
@@ -195,3 +211,28 @@ section#contact(
                 d="M20.563 3.34a1.002 1.002 0 0 0-.989-.079l-17 8a1 1 0 0 0 .026 1.821L8 15.445v6.722l5.836-4.168 4.764 2.084a1 1 0 0 0 1.399-.85l1-15a1.005 1.005 0 0 0-.436-.893zm-2.466 14.34-5.269-2.306L16 9.167l-7.649 4.25-2.932-1.283 13.471-6.34-.793 11.886z"
               )
 </template>
+
+<script>
+export default {
+  mounted: () => {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      let myForm = document.getElementById("contact-form");
+      let formData = new FormData(myForm);
+      console.log(formData)
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      })
+        .then(
+          () => console.log("Form successfully submitted")
+        )
+        .catch((error) => alert(error));
+    };
+    document
+      .querySelector("form")
+      .addEventListener("submit", handleSubmit);
+  }
+}
+</script>
